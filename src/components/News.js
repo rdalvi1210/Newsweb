5a7e7fd20f8d59f21f5d91212c4c9fb825a88ab3
Ramchandra <rdalvi1210@gmail.com>
2025-04-30 04:01:02 +0530

    first commit

---
+++ src/components/News.js
@@ -0,0 +1,117 @@
+import PropTypes from "prop-types";
+import React, { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+
+const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  const updateNews = async () => {
+    props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+
+    setLoading(true);
+    try {
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      props.setProgress(70);
+
+      if (parsedData.articles) {
+        const trimmedArticles = parsedData.articles.slice(1); // Remove first article
+        setArticles(trimmedArticles);
+        setTotalResults((parsedData.totalResults || 0) - 1); // Adjust totalResults
+      } else {
+        setArticles([]);
+        setTotalResults(0);
+      }
+    } catch (error) {
+      console.error("Error fetching news:", error);
+      setArticles([]);
+      setTotalResults(0);
+    }
+    setLoading(false);
+    props.setProgress(100);
+  };
+
+  useEffect(() => {
+    document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
+    updateNews();
+    // eslint-disable-next-line
+  }, []);
+
+  const fetchMoreData = async () => {
+    const nextPage = page + 1;
+    setPage(nextPage);
+
+    const url = `https://newsapi.org/v2/top-headlines?category=${props.category}&apiKey=${props.apiKey}&page=${nextPage}&pageSize=${props.pageSize}`;
+
+    try {
+      let data = await fetch(url);
+      let parsedData = await data.json();
+
+      if (parsedData.articles) {
+        setArticles((prevArticles) => [
+          ...prevArticles,
+          ...parsedData.articles,
+        ]);
+        setTotalResults(parsedData.totalResults || 0); // No need to subtract here
+      }
+    } catch (error) {
+      console.error("Error fetching more news:", error);
+    }
+  };
+
+  return (
+    <>
+      <h1
+        className="text-center"
+        style={{ margin: "35px 0px", marginTop: "90px" }}
+      >
+        NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines
+      </h1>
+      {loading && <Spinner />}
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length < totalResults}
+        loader={<Spinner />}
+      >
+        <div className="container">
+          <div className="row">
+            {articles.map((element, index) => (
+              <div className="col-md-4" key={index}>
+                <NewsItem
+                  title={element.title || ""}
+                  description={element.description || ""}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                  author={element.author || "Unknown"}
+                  date={element.publishedAt}
+                  source={element.source?.name || "Unknown"}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </InfiniteScroll>
+    </>
+  );
+};
+
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+  apiKey: PropTypes.string.isRequired,
+  setProgress: PropTypes.func.isRequired,
+};
+
+export default News;
